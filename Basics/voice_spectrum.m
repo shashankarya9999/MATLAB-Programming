4711c2b84d35753702eab409cfc4ee64b3c6e95b
Shashank Arya <shashankarya9831@gmail.com>
2025-05-04 11:41:35 +0530

    Update voice_spectrum.m

--- Basics/voice_spectrum.m
+++ Basics/voice_spectrum.m
@@ -1,5 +1,5 @@
 clear;
-% audioread() reads the audio file
+% audioread(): reads the audio file
 % inputAudio contains the audio samples(a vector or matrix, depending on mono/stereo)
 % fs is sampling frequency(e.g. 44100 Hz for standard audio)
 [inputAudio, fs] = audioread('Copy_of_Recording.mp3');
@@ -26,6 +26,7 @@ filtered_filename = 'filter.mp3';
 audiowrite(filtered_filename, filteredAudio, fs);
 % plotting the original audio, noisy audio and filtered audio
 % t = (0:l-1)*(1/fs);
+
 figure;
 subplot(3,1,1);
 plot((fs/l)*(-l/2:l/2-1),abs(fftshift(fft(inputAudio))),'k');
@@ -33,12 +34,14 @@ title('Original Audio');
 xlabel('Frequency');
 ylabel('Magnitude');
 ylim([0 400]);
+
 subplot(3,1,2);
 plot((fs/l)*(-l/2:l/2-1),abs(fftshift(fft(noisyAudio))),'k');
 title('Noisy Audio');
 xlabel('Frequency');
 ylabel('Magnitude');
 ylim([0 400]);
+
 subplot(3,1,3);
 plot((fs/l)*(-l/2:l/2-1),abs(fftshift(fft(filteredAudio))),'k');
 title('Filtered Audio')
